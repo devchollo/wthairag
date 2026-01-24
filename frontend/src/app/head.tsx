f4b461277d2e354c2dce109d6e491bfb0e090ca7
Kent Sevillejo <devchollo@gmail.com>
2026-01-25 01:46:07 +0800

    Update og image domain

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta property="og:image" content="https://worktoolshub.info/og-image.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+    </>
+  );
+}
