8be940eb5f6e28c38830282040d74cf7c9f7a688
Dev Chollo <devchollo@gmail.com>
2026-01-25 01:56:36 +0800

    added fb app id

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -4,6 +4,7 @@ export default function Head() {
       <meta property="og:image" content="https://ksevillejo.s3.us-east-005.backblazeb2.com/WorkToolsHub+Logo.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <meta property="fb:app_id" content="892769527065148" />
     </>
   );
 }
