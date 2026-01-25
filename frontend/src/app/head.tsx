ca5ae7ace4921a38a34f3a0b1546ec21fb2baa56
Dev Chollo <devchollo@gmail.com>
2026-01-25 13:33:40 +0800

    GA4

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -1,6 +1,17 @@
 export default function Head() {
   return (
     <>
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-2DKN72DYLE"></script>
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2DKN72DYLE');
+        `}
+      </script>
       <meta property="og:image" content="https://ksevillejo.s3.us-east-005.backblazeb2.com/WorkToolsHub+Logo.png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
